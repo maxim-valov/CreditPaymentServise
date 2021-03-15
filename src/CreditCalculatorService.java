ac4d48139a828c44a92506e8caa7544e86d72ebe
Maxim Valov <max-valov@yandex.ru>
2021-03-15 10:40:13 +0300

    изменён project

--- src/CreditCalculatorService.java
+++ src/CreditCalculatorService.java
@@ -1,7 +1,8 @@
 public class CreditCalculatorService {
     public double calculate (int sum,int period,double rateYear){
         double rateMont = rateYear / 100 / 12;
-        double payment = (sum * ((rateMont * (Math.pow((1 + rateMont), period)))
+        double payment;
+        payment= (sum * ((rateMont * (Math.pow((1 + rateMont), period)))
                         / (Math.pow((1 + rateMont), period) - 1)));
         return payment;
     }
--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,14 @@ public class Main {
     public static void main(String[] args) {
         CreditCalculatorService service = new CreditCalculatorService();
         int sum = 1_000_000;
-        int period = 36;
         double rateYear = 9.99;
-        double payment = service.calculate(sum, period, rateYear);
-        System.out.println(payment);
+        double payment1year= service.calculate(sum, 12, rateYear);
+        System.out.printf("Ежемесячный платёж срок 12 месяцев: %.0f \n", payment1year);
+
+        double payment2year= service.calculate(sum, 24, rateYear);
+        System.out.printf("Ежемесячный платёж срок 24 месяцев: %.0f \n", payment2year);
+
+        double payment3year= service.calculate(sum, 36, rateYear);
+        System.out.printf("Ежемесячный платёж срок 36 месяцев: %.0f", payment3year);
     }
 }
